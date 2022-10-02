c94e8d777563f40c58c44fac52279fb141beea47
Ankur Gupta <ankurgupta@Ankurs-Macbook-Pro-13.local>
2022-10-03 00:51:53 +0530

    Added Code Formatting using Prettier

--- public/js/app.js
+++ public/js/app.js
@@ -1,50 +1,49 @@
-const WeatherForm=document.querySelector('form')
-const search=document.querySelector('input')
-const message1=document.querySelector('#first')
-const message2=document.querySelector('#second')
+const WeatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const message1 = document.querySelector("#first");
+const message2 = document.querySelector("#second");
 
-console.log("Hey")
+console.log("Hey");
 
-fetch('http://puzzle.mead.io/puzzle').then((response)=>{
-	 response.json().then((data)=>{
-	 	console.log(data);
-	 })
-})
+fetch("http://puzzle.mead.io/puzzle").then((response) => {
+  response.json().then((data) => {
+    console.log(data);
+  });
+});
 
-fetch('http://localhost:3000/weather?address=Boston').then((response)=>{
-	if(response.error){
-		console.log(response.error);
-	}
-	else{
-		response.json().then((data)=>{
-	 	console.log(data.location);
-	 	console.log(data.forecast)
-	 })
-	}
-})
+fetch("http://localhost:3000/weather?address=Boston").then((response) => {
+  if (response.error) {
+    console.log(response.error);
+  } else {
+    response.json().then((data) => {
+      console.log(data.location);
+      console.log(data.forecast);
+    });
+  }
+});
 
-WeatherForm.addEventListener('submit',(event)=>{
-	event.preventDefault()
-	const location=search.value
-	message1.textContent='Loading....'
-	message2.textContent=''
-	fetch('http://localhost:3000/weather?address='+location).then((response)=>{
-	if(response.error){
-		console.log(response.error);
-		message1.textContent=data.error
-
-	}
-	response.json().then((data)=>{
-		if(data.error){
-			console.log(data.error);
-			message1.textContent=data.error
-		}
-		else{
-			message1.textContent=data.location
-			message2.textContent=data.forecast
-			console.log(data.location)
-			console.log(data.forecast)
-		}
- })
-})
-})
+WeatherForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const location = search.value;
+  message1.textContent = "Loading....";
+  message2.textContent = "";
+  fetch("http://localhost:3000/weather?address=" + location).then(
+    (response) => {
+      if (response.error) {
+        console.log(response.error);
+        message1.textContent = data.error;
+      }
+      response.json().then((data) => {
+        if (data.error) {
+          console.log(data.error);
+          message1.textContent = data.error;
+        } else {
+          message1.textContent = data.location;
+          message2.textContent = data.forecast;
+          console.log(data.location);
+          console.log(data.forecast);
+        }
+      });
+    }
+  );
+});
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,20 +1,31 @@
-const request=require('request')
+const request = require("request");
 
-const forecast=(latitude,longitude,callback)=>{
-	const url='https://api.darksky.net/forecast/{APIKEY}/' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude) + '?units=si'
-	request({url: url, json: true},(error,response)=>{
-	if(error){
-		callback('Unable to connect to weather service',undefined)
-	}
-	else if(response.body.error){
-		callback('Unable to find Location',undefined)
-	}
-	else{
-		const temperature=response.body.currently.temperature
-		const precipProbability=response.body.currently.precipProbability
-		const summary=response.body.daily.data[0].summary
-		callback(undefined,summary + ' It is currently '+ temperature + ' degrees out. There is a ' + precipProbability + '% chance of rain')
-	}
-})
-}
-module.exports=forecast
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "https://api.darksky.net/forecast/{APIKEY}/" +
+    encodeURIComponent(latitude) +
+    "," +
+    encodeURIComponent(longitude) +
+    "?units=si";
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback("Unable to connect to weather service", undefined);
+    } else if (response.body.error) {
+      callback("Unable to find Location", undefined);
+    } else {
+      const temperature = response.body.currently.temperature;
+      const precipProbability = response.body.currently.precipProbability;
+      const summary = response.body.daily.data[0].summary;
+      callback(
+        undefined,
+        summary +
+          " It is currently " +
+          temperature +
+          " degrees out. There is a " +
+          precipProbability +
+          "% chance of rain"
+      );
+    }
+  });
+};
+module.exports = forecast;
